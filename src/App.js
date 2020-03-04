9fe9d86784e98457cb2925f6c6f3c699ce1c41ab
Kaj Siebert <kaj@k-si.com>
2020-03-04 11:09:46 +0000

    Add pyodide

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+window.registerPyodide = (pyodide) => {
+
+}
+
+
 function App() {
+  const [pyodide, setPyodide] = useState();
+  const [pyodideReady, setPyodideReady] = useState(false);
+
+  const registerPyodide = (pyodide) => {
+    console.log("Registering pyodide in React", Object.keys(pyodide));
+    setPyodide(pyodide);
+
+    // WARNING - THIS IS HORRIBLE HACK
+    // would be nice for pyodide to provide proper callback for when ready
+    const postRun = pyodide.postRun;
+    pyodide.postRun = async function() {
+      await postRun();
+      setPyodideReady(true);
+    }
+  };
+
+  useEffect(() => {
+    window.registerPyodide = registerPyodide;
+  });
+
+  useEffect(() => {
+    if (pyodideReady) {
+      console.log("Running a bit of python");
+      const result = pyodide.runPythonAsync("print('hello world from python')")
+      console.log('PYTHON OUTPUT', result);
+    }
+  }, [pyodide, pyodideReady]);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        { pyodide && (
+            <h1>PYODIDE AVAILABLE</h1>
+        )}
+        { pyodideReady && (
+            <h2>PYODIDE Ready</h2>
+        )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
