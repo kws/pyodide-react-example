3f096f3b502011b2cb52fcdf3026cf693418c565
Kaj Siebert <kaj@k-si.com>
2020-03-04 12:52:10 +0000

    This would be the more correct way of initalising pyodide

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-
-window.registerPyodide = (pyodide) => {
-
-}
-
-
 function App() {
   const [pyodide, setPyodide] = useState();
-  const [pyodideReady, setPyodideReady] = useState(false);
 
-  const registerPyodide = (pyodide) => {
+  const registerPyodide = async pyodide => {
     console.log("Registering pyodide in React", Object.keys(pyodide));
     setPyodide(pyodide);
-
-    // WARNING - THIS IS HORRIBLE HACK
-    // would be nice for pyodide to provide proper callback for when ready
-    const postRun = pyodide.postRun;
-    pyodide.postRun = async function() {
-      await postRun();
-      setPyodideReady(true);
-    }
   };
 
   useEffect(() => {
     window.registerPyodide = registerPyodide;
-  });
-
-  useEffect(() => {
-    if (pyodideReady) {
-      console.log("Running a bit of python");
-      const result = pyodide.runPythonAsync("print('hello world from python')")
-      console.log('PYTHON OUTPUT', result);
+    if (window.pyodide) {
+      setPyodide(pyodide);
     }
-  }, [pyodide, pyodideReady]);
-
+  }, [pyodide]);
 
 
   return (
     <div className="App">
       <header className="App-header">
         { pyodide && (
+            <>
             <h1>PYODIDE AVAILABLE</h1>
-        )}
-        { pyodideReady && (
-            <h2>PYODIDE Ready</h2>
+            <p>{pyodide.runPython("'Hello World from PYTHON'")}.</p>
+            </>
         )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
